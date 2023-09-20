3931af7d1bcdd64e85e1f662b803f362a4c93694
no <no@no.com>
2023-09-20 19:34:12 +0800

    feat: add jump function
    jumpForce is a fixed positive number, as Y vector to rb.velocity

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,8 +4,11 @@ public class Player : MonoBehaviour
 {
     public Rigidbody2D rb;
 
-    public float moveSpeed = 10;
     public float xInput;
+    public float moveSpeed = 10;
+    public float jumpForce = 7;
+
+
 
     // Start is called before the first frame update
     void Start()
@@ -20,5 +23,9 @@ public class Player : MonoBehaviour
 
         rb.velocity = new Vector2(xInput * moveSpeed, rb.velocity.y);
 
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
     }
 }
