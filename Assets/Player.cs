0bde2c50ac35d1ecad4965be8f67bc9d3463869a
no <no@no.com>
2023-09-20 19:03:41 +0800

    feat: add xInput value to Player code

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,18 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float xInput;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        xInput = Input.GetAxis("Horizontal");
     }
 }
