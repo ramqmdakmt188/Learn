ad44e9f0a1a5803d52fb452228dd79342f0d177c
no <no@no.com>
2023-09-20 19:29:19 +0800

    feat: set moveSpeed to 10, use xInput and moveSpeed to change it's X Vector

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public Rigidbody2D rb;
+
+    public float moveSpeed = 10;
     public float xInput;
 
     // Start is called before the first frame update
@@ -15,5 +17,8 @@ public class Player : MonoBehaviour
     void Update()
     {
         xInput = Input.GetAxis("Horizontal");
+
+        rb.velocity = new Vector2(xInput * moveSpeed, rb.velocity.y);
+
     }
 }
