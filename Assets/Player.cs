1183788cca00f02602a7cf2ea325e52a9899f770
no <no@no.com>
2023-09-20 22:04:54 +0800

    feat: bind rb to objects' rigidbody2D

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -11,7 +11,7 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -27,4 +27,3 @@ public class Player : MonoBehaviour
         }
     }
 }
-// test internet, to be deleted...
